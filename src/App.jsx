25a6788ad8f592ce9de7a642b4f1c05e1ad13799
shubham bambharoliya <shubham2892001@gmail.com>
2025-04-24 15:01:09 +0530

    update the design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Papaparse from 'papaparse';
 import './App.css';
 
 function App() {
@@ -58,9 +59,9 @@ function App() {
 
     if (allFilters.discounted)
       data = data.filter(
-        (item) =>
-          String(item.is_discounted).toLowerCase() ===
-          allFilters.discounted.toLowerCase()
+          (item) =>
+              String(item.is_discounted).toLowerCase() ===
+              allFilters.discounted.toLowerCase()
       );
 
     // Sort
@@ -84,79 +85,111 @@ function App() {
     }
 
     setFilteredData(data);
-  }, [allFilters, sortingBy, sortingOrder, extractedData]);  
+  }, [allFilters, sortingBy, sortingOrder, extractedData]);
+
 
   return (
-    <div>
-      <h2 className='text-3xl font-bold underline'>Upload Your Data File Here .csv or .json</h2>
-      <input type="file" accept=".csv,.json" onChange={handleFileUpload} />
       <div>
-        <select onChange={(e) => setAllFilters({ ...allFilters, category: e.target.value })}>
-          <option value="">Categories</option>
-          {[...new Set(extractedData.map((item) => item.category))].map((cat) => (
-            <option key={cat}>{cat}</option>
-          ))}
-        </select>
-
-        <select onChange={(e) => setAllFilters({ ...allFilters, brand: e.target.value })} >
-          <option value="">Brands</option>
-          {[...new Set(extractedData.map((item) => item.brand))].map((brand) => (
-            <option key={brand}>{brand}</option>
-          ))}
-        </select>
-
-        <select onChange={(e) => setAllFilters({ ...allFilters, discounted: e.target.value })} >
-          <option value="">Discounted?</option>
-          <option value="true">Yes</option>
-          <option value="false">No</option>
-        </select>
-      </div>
-
-      <div>
-        <label>Sort by:</label>
-        <select
-          onChange={(e) => setSortingBy(e.target.value)}
-          
-        >
-          <option value="">None</option>
-          <option value="product_name">Name</option>
-          <option value="price">Price</option>
-          <option value="rating">Rating</option>
-        </select>
-
-        <button onClick={() => setSortingOrder((prev) => (prev === 'asc' ? 'desc' : 'asc'))} >
-          Order ({sortingOrder})
-        </button>
-      </div>
-
-      <div>
-        <table border="1" cellPadding="8" cellSpacing="0">
-          <thead>
-            <tr>
-              {filteredData.length > 0 &&
-                Object.keys(filteredData[0]).map((key) => (
-                  <th key={key}>{key}</th>
+        {extractedData.length === 0 ? (
+            <section className='max-w-md mx-auto'>
+          <h2 className='text-lg font-semibold text-center mb-4'>Upload Your Data File Here .csv or .json</h2>
+          <label
+              htmlFor="file-upload"
+              className="border-2 border-dashed border-gray-300 rounded-lg p-8 flex flex-col items-center justify-center text-center space-y-2 cursor-pointer hover:border-blue-400 transition-colors"
+          >
+          <span className='flex w-[24px] ml-2 h-[24px] items-center justify-center'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M12 12V4m0 0l-4 4m4-4l4 4" />
+            </svg>
+          </span>
+            <p className="text-sm text-gray-600">
+              <span className="font-medium text-white">Drop your file here or </span>
+              <span className="text-white underline">Browse</span>
+            </p>
+            <p className="text-xs text-gray-400">Maximum file size 50mb</p>
+            <input
+                id="file-upload"
+                type="file"
+                accept=".csv,.json"
+                onChange={handleFileUpload}
+                className="hidden"
+            />
+          </label>
+        </section>
+        ) : (
+            <section>
+          <div className='flex justify-between items-center space-x-4 '>
+            <div className="flex space-x-4">
+              <select className="py-2 px-4 rounded-[10px] bg-blend-darken border-0" onChange={(e) => setAllFilters({ ...allFilters, category: e.target.value })}>
+                <option value="">Categories</option>
+                {[...new Set(extractedData.map((item) => item.category))].map((cat) => (
+                    <option key={cat}>{cat}</option>
                 ))}
-            </tr>
-          </thead>
-          <tbody>
-            {filteredData.map((item, idx) => (
-              <tr key={idx}>
-                {Object.values(item).map((val, i) => (
-                  <td key={i}>
-                    {val?.toString().startsWith('http') ? (
-                      <img src={val} alt="img" width="50" />
-                    ) : (
-                      val
-                    )}
-                  </td>
+              </select>
+              <select className="py-2 px-4 rounded-[10px] bg-blend-darken border-0" onChange={(e) => setAllFilters({ ...allFilters, brand: e.target.value })} >
+                <option value="">Brands</option>
+                {[...new Set(extractedData.map((item) => item.brand))].map((brand) => (
+                    <option key={brand}>{brand}</option>
                 ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+              </select>
+              <select className="py-2 px-4 rounded-[10px] bg-blend-darken border-0" onChange={(e) => setAllFilters({ ...allFilters, discounted: e.target.value })} >
+                <option value="">Discounted</option>
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+              </select>
+            </div>
+            <div className="flex space-x-4 items-center">
+              <label className='text-[14px]' for="">Sort by:</label>
+              <select className="py-2 px-4 rounded-[10px] bg-blend-darken border-0"
+                      onChange={(e) => setSortingBy(e.target.value)}>
+                <option value="">None</option>
+                <option value="product_name">Name</option>
+                <option value="price">Price</option>
+                <option value="rating">Rating</option>
+              </select>
+              {/*<button className="py-2 px-4 rounded-bl bg-blend-darken border-0" onClick={() => setSortingOrder((prev) => (prev === 'asc' ? 'desc' : 'asc'))} >*/}
+              {/*  Order ({sortingOrder})*/}
+              {/*</button>*/}
+              <label className="inline-flex items-center cursor-pointer">
+                <input type="checkbox" value={sortingOrder === 'asc'} className="sr-only peer" onClick={() => setSortingOrder((prev) => (prev === 'asc' ? 'desc' : 'asc'))}/>
+                <div
+                    className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Order {sortingOrder}</span>
+              </label>
+            </div>
+          </div>
+
+              <div className="mt-8">
+                <table className="min-w-full table-auto border-collapse text-sm text-left">
+                  <thead>
+                  <tr className="bg-gray-100 text-gray-700">
+                    {filteredData.length > 0 &&
+                        Object.keys(filteredData[0]).map((key) => (
+                            <th className="p-4" key={key}>{key}</th>
+                        ))}
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {filteredData.map((item, idx) => (
+                      <tr className="border-b" key={idx}>
+                        {Object.values(item).map((val, i) => (
+                            <td className='p-4 text-xs text-white' key={i}>
+                              {val?.toString().startsWith('http') ? (
+                              <img src={val} alt="img" width="50" height="50" className='rounded-full' />
+                          ) : (
+                              val
+                          )}
+                        </td>
+                    ))}
+                  </tr>
+              ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
+        )}
+
       </div>
-    </div>
   )
 }
 
