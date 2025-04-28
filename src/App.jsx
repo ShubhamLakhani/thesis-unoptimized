fb0fbfd398d3084dc967c7cfe502b8aa8e531f74
ShubhamLakhani <lakhanishubnham9750@gmail.com>
2025-04-28 16:04:53 +0530

    pushed css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import Papaparse from 'papaparse';
-import './App.css';
+import { useEffect, useState } from "react";
+import Papaparse from "papaparse";
+import "./App.css";
 
 function App() {
   const [extractedData, setExtractedData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [allFilters, setAllFilters] = useState({
-    category: '',
-    brand: '',
-    discounted: '',
+    category: "",
+    brand: "",
+    discounted: "",
   });
-  const [sortingBy, setSortingBy] = useState('');
-  const [sortingOrder, setSortingOrder] = useState('asc');
+  const [sortingBy, setSortingBy] = useState("");
+  const [sortingOrder, setSortingOrder] = useState("asc");
 
   useEffect(() => {
-    const dataSet = localStorage.getItem('data_set');
+    const dataSet = localStorage.getItem("data_set");
     if (dataSet) {
       setExtractedData(JSON.parse(dataSet));
     }
@@ -26,22 +26,22 @@ function App() {
 
     const reader = new FileReader();
 
-    if (file.name.endsWith('.csv')) {
+    if (file.name.endsWith(".csv")) {
       Papaparse.parse(file, {
         header: true,
         skipEmptyLines: true,
         complete: function (results) {
-          localStorage.setItem('data_set', JSON.stringify(results.data));
+          localStorage.setItem("data_set", JSON.stringify(results.data));
           setExtractedData(results.data);
-          setFilteredData(results.data)
+          setFilteredData(results.data);
         },
       });
-    } else if (file.name.endsWith('.json')) {
+    } else if (file.name.endsWith(".json")) {
       reader.onload = (event) => {
         const jsonData = JSON.parse(event.target.result);
-        localStorage.setItem('data_set', JSON.stringify(jsonData));
+        localStorage.setItem("data_set", JSON.stringify(jsonData));
         setExtractedData(jsonData);
-        setFilteredData(jsonData)
+        setFilteredData(jsonData);
       };
       reader.readAsText(file);
     }
@@ -59,9 +59,9 @@ function App() {
 
     if (allFilters.discounted)
       data = data.filter(
-          (item) =>
-              String(item.is_discounted).toLowerCase() ===
-              allFilters.discounted.toLowerCase()
+        (item) =>
+          String(item.is_discounted).toLowerCase() ===
+          allFilters.discounted.toLowerCase()
       );
 
     // Sort
@@ -70,7 +70,7 @@ function App() {
         let valA = a[sortingBy];
         let valB = b[sortingBy];
 
-        if (sortingBy === 'price' || sortingBy === 'rating') {
+        if (sortingBy === "price" || sortingBy === "rating") {
           valA = parseFloat(valA);
           valB = parseFloat(valB);
         } else {
@@ -78,8 +78,8 @@ function App() {
           valB = valB?.toString().toLowerCase();
         }
 
-        if (valA < valB) return sortingOrder === 'asc' ? -1 : 1;
-        if (valA > valB) return sortingOrder === 'asc' ? 1 : -1;
+        if (valA < valB) return sortingOrder === "asc" ? -1 : 1;
+        if (valA > valB) return sortingOrder === "asc" ? 1 : -1;
         return 0;
       });
     }
@@ -87,110 +87,162 @@ function App() {
     setFilteredData(data);
   }, [allFilters, sortingBy, sortingOrder, extractedData]);
 
-
   return (
-      <div>
-        {extractedData.length === 0 ? (
-            <section className='max-w-md mx-auto'>
-          <h2 className='text-lg font-semibold text-center mb-4'>Upload Your Data File Here .csv or .json</h2>
+    <div>
+      {extractedData.length === 0 ? (
+        <section className="max-w-md mx-auto">
+          <h2 className="text-lg font-semibold text-center mb-4">
+            Upload Your Data File Here .csv or .json
+          </h2>
           <label
-              htmlFor="file-upload"
-              className="border-2 border-dashed border-gray-300 rounded-lg p-8 flex flex-col items-center justify-center text-center space-y-2 cursor-pointer hover:border-blue-400 transition-colors"
+            htmlFor="file-upload"
+            className="border-2 border-dashed border-gray-300 rounded-lg p-8 flex flex-col items-center justify-center text-center space-y-2 cursor-pointer hover:border-blue-400 transition-colors"
           >
-          <span className='flex w-[24px] ml-2 h-[24px] items-center justify-center'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M12 12V4m0 0l-4 4m4-4l4 4" />
-            </svg>
-          </span>
+            <span className="flex w-[24px] ml-2 h-[24px] items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M12 12V4m0 0l-4 4m4-4l4 4"
+                />
+              </svg>
+            </span>
             <p className="text-sm text-gray-600">
-              <span className="font-medium text-white">Drop your file here or </span>
-              <span className="text-white underline">Browse</span>
+              <span className="font-medium">
+                Drop your file here or{" "}
+              </span>
+              <span className=" underline">Browse</span>
             </p>
             <p className="text-xs text-gray-400">Maximum file size 50mb</p>
             <input
-                id="file-upload"
-                type="file"
-                accept=".csv,.json"
-                onChange={handleFileUpload}
-                className="hidden"
+              id="file-upload"
+              type="file"
+              accept=".csv,.json"
+              onChange={handleFileUpload}
+              className="hidden"
             />
           </label>
         </section>
-        ) : (
-            <section>
-          <div className='flex justify-between items-center space-x-4 '>
+      ) : (
+        <section>
+          <div className="flex justify-between items-center space-x-4 ">
             <div className="flex space-x-4">
-              <select className="py-2 px-4 rounded-[10px] bg-blend-darken border-0" onChange={(e) => setAllFilters({ ...allFilters, category: e.target.value })}>
+              <select
+                className="py-2 px-4 rounded-[10px] border-2 border-gray-800"
+                onChange={(e) =>
+                  setAllFilters({ ...allFilters, category: e.target.value })
+                }
+              >
                 <option value="">Categories</option>
-                {[...new Set(extractedData.map((item) => item.category))].map((cat) => (
+                {[...new Set(extractedData.map((item) => item.category))].map(
+                  (cat) => (
                     <option key={cat}>{cat}</option>
-                ))}
+                  )
+                )}
               </select>
-              <select className="py-2 px-4 rounded-[10px] bg-blend-darken border-0" onChange={(e) => setAllFilters({ ...allFilters, brand: e.target.value })} >
+              <select
+                className="py-2 px-4 rounded-[10px] border-2 border-gray-800"
+                onChange={(e) =>
+                  setAllFilters({ ...allFilters, brand: e.target.value })
+                }
+              >
                 <option value="">Brands</option>
-                {[...new Set(extractedData.map((item) => item.brand))].map((brand) => (
+                {[...new Set(extractedData.map((item) => item.brand))].map(
+                  (brand) => (
                     <option key={brand}>{brand}</option>
-                ))}
+                  )
+                )}
               </select>
-              <select className="py-2 px-4 rounded-[10px] bg-blend-darken border-0" onChange={(e) => setAllFilters({ ...allFilters, discounted: e.target.value })} >
+              <select
+                className="py-2 px-4 rounded-[10px] border-2 border-gray-800"
+                onChange={(e) =>
+                  setAllFilters({ ...allFilters, discounted: e.target.value })
+                }
+              >
                 <option value="">Discounted</option>
                 <option value="true">Yes</option>
                 <option value="false">No</option>
               </select>
             </div>
             <div className="flex space-x-4 items-center">
-              <label className='text-[14px]' for="">Sort by:</label>
-              <select className="py-2 px-4 rounded-[10px] bg-blend-darken border-0"
-                      onChange={(e) => setSortingBy(e.target.value)}>
+              <label className="text-[14px]" htmlFor="">
+                Sort by:
+              </label>
+              <select
+                className="py-2 px-4 rounded-[10px] border-2 border-gray-800"
+                onChange={(e) => setSortingBy(e.target.value)}
+              >
                 <option value="">None</option>
                 <option value="product_name">Name</option>
                 <option value="price">Price</option>
                 <option value="rating">Rating</option>
               </select>
-              {/*<button className="py-2 px-4 rounded-bl bg-blend-darken border-0" onClick={() => setSortingOrder((prev) => (prev === 'asc' ? 'desc' : 'asc'))} >*/}
+              {/*<button className="py-2 px-4 rounded-bl border-2 border-gray-800" onClick={() => setSortingOrder((prev) => (prev === 'asc' ? 'desc' : 'asc'))} >*/}
               {/*  Order ({sortingOrder})*/}
               {/*</button>*/}
               <label className="inline-flex items-center cursor-pointer">
-                <input type="checkbox" value={sortingOrder === 'asc'} className="sr-only peer" onClick={() => setSortingOrder((prev) => (prev === 'asc' ? 'desc' : 'asc'))}/>
-                <div
-                    className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
-                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Order {sortingOrder}</span>
+                <input
+                  type="checkbox"
+                  value={sortingOrder === "asc"}
+                  className="sr-only peer"
+                  onClick={() =>
+                    setSortingOrder((prev) => (prev === "asc" ? "desc" : "asc"))
+                  }
+                />
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Order {sortingOrder}
+                </span>
               </label>
             </div>
           </div>
 
-              <div className="mt-8">
-                <table className="min-w-full table-auto border-collapse text-sm text-left">
-                  <thead>
-                  <tr className="bg-gray-100 text-gray-700">
-                    {filteredData.length > 0 &&
-                        Object.keys(filteredData[0]).map((key) => (
-                            <th className="p-4" key={key}>{key}</th>
-                        ))}
-                  </tr>
-                  </thead>
-                  <tbody>
-                  {filteredData.map((item, idx) => (
-                      <tr className="border-b" key={idx}>
-                        {Object.values(item).map((val, i) => (
-                            <td className='p-4 text-xs text-white' key={i}>
-                              {val?.toString().startsWith('http') ? (
-                              <img src={val} alt="img" width="50" height="50" className='rounded-full' />
-                          ) : (
-                              val
-                          )}
-                        </td>
+          <div className="mt-8">
+            <table className="min-w-full table-auto border-collapse text-sm text-left">
+              <thead>
+                <tr className="bg-gray-100 text-gray-700">
+                  {filteredData.length > 0 &&
+                    Object.keys(filteredData[0]).map((key) => (
+                      <th className="p-4" key={key}>
+                        {key}
+                      </th>
+                    ))}
+                </tr>
+              </thead>
+              <tbody>
+                {filteredData.map((item, idx) => (
+                  <tr className="border-b" key={idx}>
+                    {Object.values(item).map((val, i) => (
+                      <td className="p-4 text-xs" key={i}>
+                        {val?.toString().startsWith("http") ? (
+                          <img
+                            src={val}
+                            alt="img"
+                            width="50"
+                            height="50"
+                            className="rounded-full"
+                          />
+                        ) : (
+                          val
+                        )}
+                      </td>
                     ))}
                   </tr>
-              ))}
+                ))}
               </tbody>
             </table>
           </div>
         </section>
-        )}
-
-      </div>
-  )
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
